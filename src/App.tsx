9b362b38e507c488859cfd5c12127c0f100d1c4f
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-02-02 09:00:44 +0600

    basic all css and file setups done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import './App.css';
 
 function App() {
-  return <div>This is a home page</div>;
+  return (
+    <main className='main-container'>
+      <div className='text-center text-[4rem] font-bold'>
+        <h1>{'OCR | Image To Text Converter'}</h1>
+        <h1>{'Open Source Free Tools'}</h1>
+      </div>
+
+      <div className='text-2xl text-center'>Lots more upcomming</div>
+    </main>
+  );
 }
 
 export default App;
